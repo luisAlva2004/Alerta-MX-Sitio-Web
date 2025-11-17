6fdc617bf1fe70602ea6a5eb7d910df1b4590fbe
Luis Alvarado <158495350+luisAlva2004@users.noreply.github.com>
2025-11-16 20:45:19 -0600

    Update index.js

--- index.js
+++ index.js
@@ -84,5 +84,5 @@ setInterval(async () => {
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, "0.0.0.0", () => {
-    console.log(`🚀 Servidor corriendo en puerto: ${PORT}`);
+    console.log(`Servidor corriendo en puerto: ${PORT}`);
 });
